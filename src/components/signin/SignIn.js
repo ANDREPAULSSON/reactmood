b891df2fe638a8141a4644c2354d7c9e6a8c653e
Andre Paulsson <paulsson_andre@hotmail.com>
2020-12-01 20:58:47 +0100

    Added CSS to signIn/login form and view

---
+++ src/components/signin/SignIn.js
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from 'react'
+import { UserContext } from '../../shared/provider/UserProvider'
+import { useHistory } from 'react-router-dom'
+import RoutingPath from '../../routes/RoutingPath'
+import BrowserCache from '../../shared/utils/BrowserCache'
+import './SignIn.css'
+
+export const SignIn = () => {
+	const history = useHistory()
+	const [username, setUsername] = useState()
+	const [, setAuthenticatedUser] = useContext(UserContext)
+
+	const login = (e) => {
+		e.preventDefault()
+		setAuthenticatedUser(username)
+		localStorage.setItem(BrowserCache.username, username)
+		history.push(RoutingPath.HomeView)
+	}
+
+	return (
+		<div className="signIn-container">
+			<form className="signIn-form">
+				<span>Username</span>
+				<input onChange={(event) => setUsername(event.target.value)} /> <br />
+				<span>Password</span>
+				<input type="password"/> <br />
+				<button onClick={(e) => login(e)}>Login</button>
+			</form>
+		</div>
+	)
+}
