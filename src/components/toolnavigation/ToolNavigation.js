95656182782e8da18d36963188d4a3221c364b10
Andre Paulsson <paulsson_andre@hotmail.com>
2020-12-01 16:02:51 +0100

    Fixed CSS for toolnav + jokebox

--- src/components/toolnavigation/ToolNavigation.js
+++ src/components/toolnavigation/ToolNavigation.js
@@ -4,17 +4,17 @@ import {JokeBox} from '../jokebox/JokeBox'
 import 'font-awesome/css/font-awesome.min.css'
 
 export const ToolNavigation = () => {
-	// const history = useHistory()
 	const [visible, setVisible] = useState(true)
+	const [active, setActive] = useState(false)
 
 	return (
 
 		<div className="tool-box">
 			<div className="tool-nav">
 				<div className="tool">
-					<span className="arrow-down"><i className="fa fa-angle-double-down"></i>	</span>		
-					<span className ="tool-name jokebox"onClick={()=> setVisible(!visible)}>Jokebox</span>	
-					<div className={visible ? 'tool-drop display-none' : 'tool-drop display-block'}>
+					<span className={active ? 'arrow-down active' : 'arrow-down'}><i className="fa fa-angle-double-down"></i>	</span>		
+					<span className ={active ? 'tool-name active' : 'tool-name'} onClick={()=>{ setVisible(!visible); setActive(!active)}}>Jokebox</span>	
+					<div className={visible ? 'tool-drop display-none' : 'tool-drop display-flex'}>
 						<JokeBox/>
 					</div>									
 				</div>		
