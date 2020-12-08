a5eb88085aff60de9b37bf03b346f6a82a566fb2
Andre Paulsson <paulsson_andre@hotmail.com>
2020-12-08 02:34:56 +0100

    .

--- src/components/boxes/quotebox/QuoteBox.js
+++ src/components/boxes/quotebox/QuoteBox.js
@@ -1,5 +1,4 @@
 import React, {useEffect,useState} from 'react'
-//import {useFetch} from '../../../hooks/useFetch'
 import QuoteService from '../../../shared/api/service/quoteService'
 import QuoteTypeValue from './QuoteTypeValue'
 import '../../boxes/Boxes.css'
